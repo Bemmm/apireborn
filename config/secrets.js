d9d6eac47b21746975e5b5e63ad0f63efd40a441
Bogdan <bembenok@gmail.com>
2017-07-04 12:57:35 +0300

    db connect

--- config/secrets.js
+++ config/secrets.js
@@ -6,7 +6,8 @@ const mb = 1024 * 1024;
 let config = {
     production: {
         port: process.env.PORT,
-        db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://13.95.149.21:27017/lexterr',
+        // db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://13.95.149.21:27017/lexterr',
+        db: 'mongodb://lexter:qwerty@ds147052.mlab.com:47052/lexter_dev',
         sessionSecret: process.env.SESSION_SECRET || 'Dshtrih2',
         tokenSecret: process.env.TOKEN_SECRET || 'Dshtrih2',
         files: { dir: path.join(__dirname, '../public/uploads/'), size: mb * 3, count: 8 },
